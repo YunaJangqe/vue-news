bf6d82f9088ddb64c24aef28ef5662d2cc68240b
jyn <lis3277@gmail.com>
2024-08-28 20:29:12 +0900

    commit

--- src/api/index.js
+++ src/api/index.js
@@ -9,8 +9,13 @@ const config = {
 function fetchNewsList() {
     return axios.get(`${config.baseUrl}news/1.json`);
 }
-function fetchAskList() {
-    return axios.get(`${config.baseUrl}ask/1.json`);
+async function fetchAskList() {
+    try {
+        const response = await axios.get(`${config.baseUrl}ask/1.json`);
+        return response;
+    } catch(error) {
+        console.log(error);
+    }    
 }
 function fetchJobsList() {
     return axios.get(`${config.baseUrl}jobs/1.json`);
@@ -21,8 +26,13 @@ function fetchUserInfo(username) {
 function fetchCommentItem(id) {
     return axios.get(`${config.baseUrl}item/${id}.json`);
 }
-function fetchList(pageName) {
-    return axios.get(`${config.baseUrl}${pageName}/1.json`);
+async function fetchList(pageName) {
+    try {
+        const response = await axios.get(`${config.baseUrl}${pageName}/1.json`);
+        return response;
+    } catch(error) {
+        console.log(error);   
+    }
 }
 
 export {
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,58 +1,48 @@
 import { fetchNewsList, fetchAskList, fetchJobsList, fetchUserInfo, fetchCommentItem, fetchList } from '../api/index.js';
 
 export default {
-    FETCH_NEWS(context) {
-        return fetchNewsList()
-        .then(respnose => {
-            console.log(respnose);
-            context.commit('SET_NEWS', respnose.data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    // FETCH_NEWS(context) {
+    //     return fetchNewsList()
+    //     .then(respnose => {
+    //         console.log(respnose);
+    //         context.commit('SET_NEWS', respnose.data);
+    //     })
+    //     .catch(error => {
+    //         console.log(error);
+    //     });
+    // },
+    async FETCH_NEWS({ commit }) {
+        const response = await fetchNewsList();
+        commit('SET_NEWS', response.data);
+        return response;
     },
-    FETCH_ASK(context) {
-        return fetchAskList()
-        .then(response => {
-            context.commit('SET_ASK', response.data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
+    async FETCH_ASK({ commit }) {
+        const response = await fetchAskList();
+        commit('SET_ASK', response.data);
+        return response;
     },
-    FETCH_JOBS({ commit }) {
-        return fetchJobsList()
-        .then(({ data }) => {
-            commit('SET_JOBS', data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    },
-    FETCH_USER({ commit }, username) {
-        return fetchUserInfo(username)
-        .then(({ data }) => {
-            commit('SET_USER', data);
-        })
-        .catch(error => {
-            console.log(error);
-        })
-    },
-    FETCH_ITEM({ commit }, id ) {
-        return fetchCommentItem(id)
-        .then(({ data }) => {
-            commit('SET_ITEM', data);
-        })
-        .catch(error => {
-            console.log(error);
-        })
-    },
-    FETCH_LIST({ commit }, pageName) {
-        return fetchList(pageName)
-        .then(response => {
-            commit('SET_LIST', response.data);
+    async FETCH_JOBS({ commit }) {
+        try {
+            const response = await fetchJobsList();
+            commit('SET_JOBS', response.data);
             return response;
-        })
-        .catch(error => console.log(error))
+        } catch(error) {
+            console.log(error);
+        }        
+    },
+    async FETCH_USER({ commit }, username) {
+        const response = await fetchUserInfo(username);
+        commit('SET_USER', response.data);
+        return response;
+    },
+    async FETCH_ITEM({ commit }, id) {
+        const response = await fetchCommentItem(id);
+        commit('SET_ITEM', response.data);
+        return response;
+    },
+    async FETCH_LIST({ commit }, pageName) {
+        const response = await fetchList(pageName);
+        commit('SET_LIST', response.data);
+        return response;
     }
 }
